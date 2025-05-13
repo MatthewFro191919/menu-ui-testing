b7ef8c063ad23f91f88421b442f36350c47ddead
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-05-13 16:28:59 -0400

    Update HealthIcon.hx

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -27,24 +27,44 @@ class HealthIcon extends FlxSprite
 	private var iconOffsets:Array<Float> = [0, 0];
 	public function changeIcon(char:String, ?allowGPU:Bool = true) {
 		if(this.char != char) {
-			var name:String = 'icons/' + char;
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
-			
-			var graphic = Paths.image(name, allowGPU);
-			loadGraphic(graphic, true, Math.floor(graphic.width / 2), Math.floor(graphic.height));
-			iconOffsets[0] = (width - 150) / 2;
-			iconOffsets[1] = (height - 150) / 2;
-			updateHitbox();
+			loadGraphic(Paths.image('gameshit/iconGrid'), true, 150, 150);
 
-			animation.add(char, [0, 1], 0, false, isPlayer);
+		if(char=='pshaggy' || char=='gshaggy' || char=='tshaggy'){
+			loadGraphic(Paths.image('shageee'), true, 150, 150);
+			antialiasing=true;
+			animation.add('pshaggy', [0, 1], 0, false);
+			animation.add('gshaggy', [2,3], 0, false);
+			animation.add('tshaggy', [2,3], 0, false);
 			animation.play(char);
-			this.char = char;
-
-			if(char.endsWith('-pixel'))
-				antialiasing = false;
+		}
+		}else{
+			loadGraphic(Paths.image('iconGrid'), true, 150, 150);
+			antialiasing=true;
+			animation.add('bf', [0, 1], 0, false);
+			animation.add('bf-car', [0, 1], 0, false);
+			animation.add('bf-christmas', [0, 1], 0, false);
+			animation.add('spooky', [2, 3], 0, false);
+			animation.add('pico', [4, 5], 0, false);
+			animation.add('mom', [6, 7], 0, false);
+			animation.add('mom-car', [6, 7], 0, false);
+			animation.add('tankman', [8, 9], 0, false);
+			animation.add('face', [10, 11], 0, false);
+			animation.add('dad', [12, 13], 0, false);
+			animation.add('bf-old', [14, 15], 0, false);
+			animation.add('gf', [16], 0, false);
+			animation.add('lizzy', [16], 0, false);
+			animation.add('parents-christmas', [17], 0, false);
+			animation.add('monster', [19, 20], 0, false);
+			animation.add('monster-christmas', [19, 20], 0, false);
+			animation.add('bf-pixel', [21, 21], 0, false);
+			animation.add('senpai', [22, 22], 0, false);
+			animation.add('senpai-angry', [22, 22], 0, false);
+			animation.add('spirit', [23, 23], 0, false);
+			animation.add('bf-neb', [24,25], 0, false);
+			if(animation.getByName(char)!=null)
+				animation.play(char);
 			else
-				antialiasing = ClientPrefs.data.antialiasing;
+				animation.play("face");
 		}
 	}
 
