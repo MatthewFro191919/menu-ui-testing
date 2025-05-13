7b462a73ce821d60ddba95549fd74278ede9a281
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-05-13 17:06:00 -0400

    Update setup-windows.bat

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -20,17 +20,16 @@ haxelib install tjson
 haxelib install hxjsonast
 haxelib install hxCodec 2.5.1
 haxelib install SScript
-haxelib git SScript https://github.com/AtonyGit/SScript-Old
 haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
-haxelib install hscript
 haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git faxe https://github.com/uhrobots/faxe
-haxelib git polymod https://github.com/larsiusprime/polymod
+haxelib git polymod https://github.com/larsiusprime/polymod.git
 haxelib install hxcpp-debug-server
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
-haxelib git SScript https://github.com/AtonyGit/SScript-Old
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc.git
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate
+haxelib remove SScript
+haxelib install SScript
 echo Finished!
 pause
